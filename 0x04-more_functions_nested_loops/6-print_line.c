15c96ced371d3be35d094b1b5a245b59aaf054bc
ifiokonko@gmail.com <ifiokonko@gmail.com>
2022-10-30 03:56:26 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - print a straight line  using '_'
+* @n: The number of '_' to be printed
+*/
+void print_line(int n)
+{
+int len;
+if (n > 0)
+{
+for (len = 0; len < n; len++)
+_putchar('_');
+}
+_putchar('\n');
+}
