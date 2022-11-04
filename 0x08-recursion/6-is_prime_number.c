e2ea72fdf99d3f52847ab985f8ecb36bc54f4e18
ifiokonko@gmail.com <ifiokonko@gmail.com>
2022-11-03 21:17:45 -0700

    function to input prime number updated

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,34 @@
 #include "main.h"
+
 /**
- * is_prime_number -  Returns the natural square root of a number.
- * @n: int type
- * Return: char type
- **/
+ * is_prime - detects if an input number is a prime number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
+int is_prime(unsigned int n, unsigned int c)
+{
+if (n % c == 0)
+{
+if (n == c)
+return (1);
+else
+return (0);
+}
+return (0 + is_prime(n, c + 1));
+}
+/**
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
 int is_prime_number(int n)
 {
-if (n <= 1)
-{
+if (n == 0)
 return (0);
-}
+if (n < 0)
+return (0);
+if (n == 1)
+return (0);
 return (is_prime(n, 2));
-
 }
-
-/**
- * is_prime - Entry point
- * Desc: is_prime
- * @n: type int
- * @x: type int
- * Return: Function about prime numbers
- **/
-int is_prime(int n, int x)
-{
-if (x < n)
-{
-if (n % x == 0)
-{
-return (0);
-}
-return (is_prime(n, x + 1));
-}
-return (1);
-
